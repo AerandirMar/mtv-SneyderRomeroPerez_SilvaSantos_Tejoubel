7faa6ab07e24896e9da6800db3749f2f9521465d
Marcelo Adrian Tejoubel <marce300983@gmail.com>
2022-05-26 16:18:37 -0300

    Añadido ../ al DIRS de settings.py

--- familia/settings.py
+++ familia/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'bdFamilia.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [Path('familia/plantillas/')],
+        'DIRS': [Path('../familia/plantillas/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
